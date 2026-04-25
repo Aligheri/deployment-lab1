4aa115efe83dbf43a996fe4f849cf4a9cbc7fdf9
ivanev <ivanyevsieiev@gmail.com>
2026-04-25 09:00:00 +0300

    chore: initialize spring boot project

---
+++ src/main/java/org/example/deploymentlab1/DeploymentLab1Application.java
@@ -0,0 +1,13 @@
+package org.example.deploymentlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentLab1Application.class, args);
+	}
+
+}
